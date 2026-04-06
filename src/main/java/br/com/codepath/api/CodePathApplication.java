ea7d46bc54c704fa75fe513c0fd275998b416357
LukazAllan <allanlucas130@gmail.com>
2026-04-06 19:23:14 -0300

    O Primeiro

---
+++ src/main/java/br/com/codepath/api/CodePathApplication.java
@@ -0,0 +1,13 @@
+package br.com.codepath.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/codepath/api/CodePathApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codepath.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
