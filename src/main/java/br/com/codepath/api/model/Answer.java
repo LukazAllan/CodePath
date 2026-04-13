597a39a14fb8070177e0490a42835d48abdaf4e6
LukazAllan <allanlucas130@gmail.com>
2026-04-13 14:08:56 -0300

    Refactored class "Answer"

--- src/main/java/br/com/codepath/api/model/Answer.java
+++ src/main/java/br/com/codepath/api/model/Answer.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "answer")
 @Data
@@ -14,6 +16,19 @@ public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "questionId")
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "selectedOptionId")
+    private Option option;
+
     private String body;
 
 }
