5f6d20b7c70445478103fc5e93cf8a6114150a5d
LukazAllan <allanlucas130@gmail.com>
2026-04-13 14:05:33 -0300

    Created class "Option"

---
+++ src/main/java/br/com/codepath/api/model/Option.java
@@ -0,0 +1,24 @@
+package br.com.codepath.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "option")
+@Entity
+@Data
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "questionId")
+    private Question question;
+
+    private String text;
+    private Boolean isCorrect;
+}
