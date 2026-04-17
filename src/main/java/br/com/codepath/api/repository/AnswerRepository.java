7a4392748150a5dce4b106a5b6a6c952cf26361e
FrankkAntonio <frankkantonio69@gmail.com>
2026-04-17 15:59:36 -0300

    Criado AnswerRepository

--- src/main/java/br/com/codepath/api/repository/AnswerRepository.java
+++ src/main/java/br/com/codepath/api/repository/AnswerRepository.java
@@ -1,4 +1,11 @@
 package br.com.codepath.api.repository;
 
-public interface AnswerRepository {
+import br.com.codepath.api.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
 }
