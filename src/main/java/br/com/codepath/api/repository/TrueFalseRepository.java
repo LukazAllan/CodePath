5ee2bb9610805ed77c30206a33de6ffd434b03ea
LukazAllan <allanlucas130@gmail.com>
2026-05-15 14:17:51 -0300

    Revove Unused Classes
    Refactoring over-engineering code
    On branch feature/refactor-question-classes
    Changes to be committed:
    	deleted:    src/main/java/br/com/codepath/api/model/AnswerMatching.java
    	modified:   src/main/java/br/com/codepath/api/model/MatchingPair.java
    	deleted:    src/main/java/br/com/codepath/api/model/Option.java
    	deleted:    src/main/java/br/com/codepath/api/model/ShortAnswer.java
    	deleted:    src/main/java/br/com/codepath/api/model/TrueFalse.java
    	deleted:    src/main/java/br/com/codepath/api/model/factories/QuestionFactory.java
    	deleted:    src/main/java/br/com/codepath/api/model/strategies/MatchingPairStrategy.java
    	deleted:    src/main/java/br/com/codepath/api/model/strategies/MultipleChoiceStrategy.java
    	deleted:    src/main/java/br/com/codepath/api/model/strategies/ShortTextStrategy.java
    	deleted:    src/main/java/br/com/codepath/api/model/strategies/TrueFalseStrategy.java
    	deleted:    src/main/java/br/com/codepath/api/repository/MatchingPairRepository.java
    	deleted:    src/main/java/br/com/codepath/api/repository/OptionRepository.java
    	deleted:    src/main/java/br/com/codepath/api/repository/ShortAnswerRepository.java
    	deleted:    src/main/java/br/com/codepath/api/repository/TrueFalseRepository.java

--- src/main/java/br/com/codepath/api/repository/TrueFalseRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.codepath.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import br.com.codepath.api.model.TrueFalse;
-
-@Repository
-public interface TrueFalseRepository extends JpaRepository<TrueFalse, Long> {
-}
