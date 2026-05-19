1b565d0e02162246651c366793d3ff1b5cc31498
LukazAllan <allanlucas130@gmail.com>
2026-05-19 10:37:25 -0300

    Fixed: typo on StreakController at @RequestMapping

--- src/main/java/br/com/codepath/api/controller/StreakController.java
+++ src/main/java/br/com/codepath/api/controller/StreakController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/streak")
+@RequestMapping("/streaks")
 public class StreakController {
 
     @Autowired
