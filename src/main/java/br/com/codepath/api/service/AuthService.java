26eff451012ed8f607ff575cf5dc64bad478e6da
LukazAllan <allanlucas130@gmail.com>
2026-05-19 20:51:33 -0300

    Refactor signUp method in AuthController and AuthService to return TokenResponseDTO

--- src/main/java/br/com/codepath/api/service/AuthService.java
+++ src/main/java/br/com/codepath/api/service/AuthService.java
@@ -9,8 +9,10 @@ import br.com.codepath.api.repository.SessionRepository;
 import br.com.codepath.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import org.sqlite.SQLiteException;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -29,7 +31,27 @@ public class AuthService {
 
     private static final long TIMEOUT_MINUTES = 5;
 
-    public void signUp(NewUserRequestDTO user) {
+    public TokenResponseDTO login(String email, String password) {
+
+        User user = userRepository
+                .findByEmailAndPassword(email, password)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        Session session = sessionRepository.findByUserId(user.getId())
+                .orElse(new Session());
+        if (session.getToken() == null) {
+            String token = UUID.randomUUID().toString();
+            session.setUser(user);
+            session.setToken(token);
+            session.setLastActivity(LocalDateTime.now());
+            sessionRepository.save(session);
+        }
+
+        return new TokenResponseDTO(session.getToken());
+    }
+
+
+    public TokenResponseDTO signUp(NewUserRequestDTO user) {
         User newUser = new User();
         newUser.setName(user.getName());
         newUser.setEmail(user.getEmail());
@@ -37,24 +59,7 @@ public class AuthService {
         newUser.setXp(0);
         newUser.setHearts(10);
         userService.create(newUser);
-    }
-
-    public TokenResponseDTO login(String email, String password) {
-
-        User user = userRepository
-                .findByEmailAndPassword(email, password)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        String token = UUID.randomUUID().toString();
-
-        Session session = new Session();
-        session.setUser(user);
-        session.setToken(token);
-        session.setLastActivity(LocalDateTime.now());
-
-        sessionRepository.save(session);
-
-        return new TokenResponseDTO(token);
+        return this.login(user.getEmail(), user.getPassword());
     }
 
     public User validateSession(String token) {
