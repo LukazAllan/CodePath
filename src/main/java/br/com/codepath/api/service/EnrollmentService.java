ad369aae2ea0c5c76551f51d50f7ebb8643d7aac
LukazAllan <allanlucas130@gmail.com>
2026-05-31 18:17:47 -0300

    Fixed EnrollmentService

--- src/main/java/br/com/codepath/api/service/EnrollmentService.java
+++ src/main/java/br/com/codepath/api/service/EnrollmentService.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.codepath.api.dto.response.AprenderResponseDTO;
 import br.com.codepath.api.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
